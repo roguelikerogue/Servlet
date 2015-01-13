585779c87824ea99b58ea9c50ddd578ca7598172
Cameron <brownlec@tcd.ie>
2015-01-13 14:02:46 +0100

    Updated format yet again

--- 2nd Copy of com.smarthouse.rest/src/com/smarthouse/rest/status/Sender.java
+++ 2nd Copy of com.smarthouse.rest/src/com/smarthouse/rest/status/Sender.java
@@ -5,7 +5,7 @@ import Client.Client;
 public class Sender {
 	
 	private String ip = "localhost";
-	private int port = 7242;//local port number 7242
+	private int port = 7239;//local port number 7239
 	//private int port = 5906; //remote port number
 	public String getResponse(String message){
 		
